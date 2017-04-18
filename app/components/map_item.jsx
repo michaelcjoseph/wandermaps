670dafa78d9bee1fef49a48c50f16e81149a1639
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-04-18 09:51:07 -0400

    Link each city to google map url

--- app/components/map_item.jsx
+++ app/components/map_item.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const MapItem = (props) => {
   return (
-    <div className="col-xs-6 col-md-4">
+    <a 
+      className="col-xs-6 col-md-4"
+      href={props.item.url} 
+      target="_blank" 
+    >
       <h1>{props.item.city}</h1>
       <h3>{props.item.country}</h3>
-    </div>
+    </a>
   );
 }
 
