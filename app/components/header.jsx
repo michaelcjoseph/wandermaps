5144a05d0b63e205e74579d3ef89b965cb646466
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-04-24 14:09:19 -0400

    Add logos and fix header responsiveness

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,7 +4,7 @@ import EmailForm from '../containers/email_form.jsx';
 const Header = (props) => {
   return (
     <header className="header-nav zindex100 position-fixed col-xs-12">
-      <div className="center-align display-flex flex-vertically-center flex-horizontally-center">
+      <div className="center-align display-flex flex-vertically-center flex-horizontally-center flex-wrap">
         <div className="position-relative">
           <p className="header-text"><strong>Don't miss out! Get updates on cities!</strong></p>
         </div>
--- app/components/title.jsx
+++ app/components/title.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+const logo = require('../assets/images/logo.svg');
 
 const Title = (props) => {
   return (
     <div className="position-absolute width100 center-align title col-xs-12">
-      <h1 className="fat-font">Wander Maps</h1>
+      <img className="title-logo" src={logo} alt="Wander Maps" />
       <h4 className="thin-font">Neighborhood Guides Crowdsourced from Locals</h4>
     </div>
   );
