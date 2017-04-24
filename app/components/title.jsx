d3f02d6a8b580d46f7848b06280125a7d8bb66b4
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-04-24 14:55:30 -0400

    Edit styling of title section and add description

--- app/components/title.jsx
+++ app/components/title.jsx
@@ -3,9 +3,20 @@ const logo = require('../assets/images/logo.svg');
 
 const Title = (props) => {
   return (
-    <div className="position-absolute width100 center-align title col-xs-12">
-      <img className="title-logo" src={logo} alt="Wander Maps" />
-      <h4 className="thin-font">Neighborhood Guides Crowdsourced from Locals</h4>
+    <div className="width100">
+      <div className="title position-absolute center-align col-xs-12">
+        <img className="title-logo" src={logo} alt="Wander Maps" />
+        <h4 className="title-subtext thin-font">
+          City guides crowdsourced from blogs, locals, travelers, and You*
+        </h4>
+      </div>
+      <div className="title-desc position-absolute">
+        <p className="thin-font">
+          *Help contribute to our community! All general city maps are openly 
+          editable so that you can add new spots or notes to make the next 
+          person's experience in that city all the better.
+        </p>
+      </div>
     </div>
   );
 }
