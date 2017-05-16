887ed43e9c4b76e8c0bb6dabe9b8a729a0287e65
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-16 12:33:22 -0700

    Remove use of nodemailer

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,33 +1,7 @@
-const nodemailer = require('nodemailer');
 const User = require('../models').user;
 
 module.exports = {
   create(req, res) {
-    var transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: 'wandermapss@gmail.com', // Your email id
-        pass: 'WanderMapsPasswordSamMike8' // Your password
-      }
-    });
-
-    var text = 'New email added to Wander Maps: ' + req.body.email;
-
-    var mailOptions = {
-      from: 'wandermapss@gmail.com', 
-      to: 'wandermapss@gmail.com', 
-      subject: 'New Wander Maps Subscriber!', 
-      text: text 
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Message sent: ' + info.response);
-      };
-    });
-
     return User
       .create({
         email: req.body.email,
