56218f0868ec06d434cf67020eeb234aef15a0d9
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-22 14:46:58 -0700

    Fix user model for creating users

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -2,9 +2,14 @@
 
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
-    email: DataTypes.STRING,
-    allowNull: false,
-    unique: true
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    }
   }, {
     classMethods: {
       associate: (models) => {
