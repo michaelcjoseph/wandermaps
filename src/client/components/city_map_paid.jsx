20d3863fc76a2bb483787924df229ef5d70d3094
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-23 16:03:59 -0700

    Add text to purchase flow

--- src/client/components/city_map_paid.jsx
+++ src/client/components/city_map_paid.jsx
@@ -7,18 +7,43 @@ const CityMapPaid = (props) => {
     return <iframe src={props.map_url} width="100%" height="100%" />;
   };
 
+  const getHeader = () => {
+    return (
+      props.show_stripe_button 
+        ? "Click the button below to purchase the map:"
+        : "To access the map, enter your email address below:"
+    )
+  }
+
+  const getDescription = () => {
+    return (
+      props.show_stripe_button 
+        ? `
+          Once you purchase the map once, you will always have access to it via
+          your email address. We only store your email address. For your 
+          privacy and security, we do not store any payment information.
+          `
+        : `
+          If you did not previously purchase this map, you'll be asked to
+          do so after entering your email address.
+          `
+    )
+  }
+
   const getEmailForm = () => {
     return (
-      <EmailForm 
-        button_text="Submit" 
-        email_subbmitted_text="Verifying email..."
-        handleSubmit={props.handleSubmit} />
+      <div className="action-item-margin col-xs-8 col-xs-offset-2">
+        <EmailForm 
+          button_text="Submit" 
+          email_subbmitted_text="Verifying email..."
+          handleSubmit={props.handleSubmit} />
+      </div>
     );
   };
 
   const getStripeButton = () => {
     return (
-      <div>
+      <div className="action-item-margin center-align col-xs-12">
         <StripeCheckout 
           token={props.handleStripePurchase}
           stripeKey="pk_live_CePqZWCB5G6DfdUj6wJudDTj"
@@ -40,7 +65,14 @@ const CityMapPaid = (props) => {
   if (props.is_map_paid) {
     return getMap();
   } else {
-    return (props.show_stripe_button ? getStripeButton() : getEmailForm());
+    return (
+      <div className="input-padding center-align col-xs-12">
+        <h2 className="col-xs-12">{getHeader()}</h2>
+        {props.show_stripe_button ? getStripeButton() : getEmailForm()}
+        <p className="col-xs-12">{getDescription()}</p>
+      </div>
+      
+    )
   }
 }
 
