abf954a7cd22de088d573c0c0a624b4a2d99c0f5
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-24 18:28:17 -0700

    Edit Not Found Page

--- src/client/components/not_found_page.jsx
+++ src/client/components/not_found_page.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Header from './header.jsx';
 
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <div className="not-found">
-        <h1>404</h1>
-        <h2>Page not found!</h2>
-        <p>
-          <Link to="/">Go back to the main page</Link>
-        </p>
+      <div className="not-found col-xs-12">
+        <Header back_button={false} email_form={false} />
+        <div className="padding-for-header-mobile main-side-padding col-xs-12">
+          <h1 className="col-xs-12">Oops!</h1>
+          <h2 className="col-xs-12">
+            Somehow you ended up on a page that's not supposed to exit.
+          </h2>
+          <p className="col-xs-12">
+            <Link to="/" className="col-xs-12">
+              Click here to go to the actual home page of Wander Maps.
+            </Link>
+          </p>
+        </div>
       </div>
     );
   }
